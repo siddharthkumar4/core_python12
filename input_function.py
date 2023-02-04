4a23325fd17237d0c85f8b6a836c98601319f0c0
siddharthkumar4 <siddharthamarco@gmail.com>
2023-02-04 17:21:01 +0530

    input_function

---
+++ input_function.py
@@ -0,0 +1,19 @@
+
+# a = input("whonis blesseed")
+# print(a)
+# c = 56
+# b = int(input("enter the number "))
+# print(b)
+
+
+l1 = []
+for i in range(0,4):
+    l1.append(int(input("enter the number")))
+
+a=0
+for p in l1:
+    a += p
+
+
+
+print(a)
