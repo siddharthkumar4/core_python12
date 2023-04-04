1342d48974252a7955f0fdcc5d347d3319e1cc7a
siddharthkumar4 <119997989+siddharthkumar4@users.noreply.github.com>
2023-04-04 21:59:48 +0530

    Update format_string.py

--- format_string.py
+++ format_string.py
@@ -1,72 +1,72 @@
 
 
-# s = "siddharth kumare shaRMA"
-# print(type(s))
-# for i in  s:
-#     print(i)
-#
-# s[0:5]
-# s[4:7]
-#
-#
-#
-#
-# str1 = "darkhast"
-# str2 = "foranci"
-# st = print(str1 + str2)
-#
-# # repetation of string
-# st = "siddharth"
-# st1 = "darkhast"
-# print(st+st1)
+s = "siddharth kumare shaRMA"
+print(type(s))
+for i in  s:
+    print(i)
+
+s[0:5]
+s[4:7]
+
+
+
+
+str1 = "darkhast"
+str2 = "foranci"
+st = print(str1 + str2)
+
+# repetation of string
+st = "siddharth"
+st1 = "darkhast"
+print(st+st1)
 
 # formatting the string (c- style formatting)
-# print("i%s feel very %s" %("siddharth","blessed"))
-#
-# print("%s is very entrepreneurial spirtit mind set"%("siddharth kumar sharma"))
-#
-# print("roll no- %d %s"%(67.78,"pass"))
-#
-# print("start-up is very good %i %s "%(4,'siddharth kumar sharma'))
-#
-# print("%i my name is siddharth"%(45))
-# print("%d %s"%(3454,'relevant'))
-#
+print("i%s feel very %s" %("siddharth","blessed"))
+
+print("%s is very entrepreneurial spirtit mind set"%("siddharth kumar sharma"))
+
+print("roll no- %d %s"%(67.78,"pass"))
+
+print("start-up is very good %i %s "%(4,'siddharth kumar sharma'))
+
+print("%i my name is siddharth"%(45))
+print("%d %s"%(3454,'relevant'))
+
 
 
 
 
 #formatting  keys and value
-# print("%(nm)d %(ky)s"%{'nm':23,'ky':"siddharth"})
+print("%(nm)d %(ky)s"%{'nm':23,'ky':"siddharth"})
 
 
 # note- jahan pe format karna hai string main wahan pe% uske baad key value (key value)  aur uske baad data type conversion
 
-#print("i m very blessed%(wd)d i feel so lucky%(df)s"%{'wd':345,'df':"grsdgbnjj"})
+print("i m very blessed%(wd)d i feel so lucky%(df)s"%{'wd':345,'df':"grsdgbnjj"})
 
 
 # using flags  and width
-#print("i m very blessed person%*7s"%("sid"))
-#print("%*15i"%567)
-#print("%.3d"%)
-#print("%15.5f"%567.67567)
+print("i m very blessed person%*7s"%("sid"))
+print("%*15i"%567)
+print("%.3d"%)
+print("%15.5f"%567.67567)
 
-# print("i m very blessed person%+30.4f"%(454.67))
-# print("i m blessed so  i m good in talking%045.2f "%567.7894)
-#
-# a = 5
-# b = 78
-# c = 67
-# print((2>1) and a and b)
-# print((1>45) and a and b)
-# print((1>5) or a or b)
+print("i m very blessed person%+30.4f"%(454.67))
+print("i m blessed so  i m good in talking%045.2f "%567.7894)
+
+a = 5
+b = 78
+c = 67
+print((2>1) and a and b)
+print((1>45) and a and b)
+print((1>5) or a or b)
 
 
 
-# print((1>5) and a and b)
-# print((5>1) and b and a)
-#print((45>1) and a or b)
-#print((45<1) or a or b and c)
+print((1>5) and a and b)
+print((5>1) and b and a)
+print((45>1) and a or b)
+print((45<1) or a or b and c)
 
 
 
@@ -77,37 +77,37 @@
 
 
 
-#print("i m {1}bles{2}sed{0}".format(24, "jfnfn","**"))
+print("i m {1}bles{2}sed{0}".format(24, "jfnfn","**"))
 
-# print("heyy i m siddharth {key1:015.2f}kumar sharma {key:s}".format(key="extraction ",key1=23.567))
-# print("hey i m blessed")
-#
-# print("extraction movie is good{key:*>15.2f}".format(key=456.546))
-#print("siddharth kumar {jack:&>30.2f}".format(jack=456.678)
+print("heyy i m siddharth {key1:015.2f}kumar sharma {key:s}".format(key="extraction ",key1=23.567))
+print("hey i m blessed")
+
+print("extraction movie is good{key:*>15.2f}".format(key=456.546))
+print("siddharth kumar {jack:&>30.2f}".format(jack=456.678)
 
 
 #formatting the integer
-# print(format(45566,"*>-20d"))
+print(format(45566,"*>-20d"))
 
-#print("my name is siddharth {0:*>15s}".format("bfhgvb"))
-#print(format(4567,"#>20d"))
-# print(format(4756756,"&>20d"))
-# print(format(75757,"(^20d"))
+print("my name is siddharth {0:*>15s}".format("bfhgvb"))
+print(format(4567,"#>20d"))
+print(format(4756756,"&>20d"))
+print(format(75757,"(^20d"))
 
 
 #formating the floating value
-# print(format(746575.756,"$<20.1f"))
-# print(format(57675.775,"#^20.1f"))
+print(format(746575.756,"$<20.1f"))
+print(format(57675.775,"#^20.1f"))
 
 # formating the number into percentage
-# print(format(57766,"10.2%"))
-# print(format(87.9,"20%"))
-# print(format(7.4,"10%"))
+print(format(57766,"10.2%"))
+print(format(87.9,"20%"))
+print(format(7.4,"10%"))
 
 # formatting in scientific Notation
-# print(format(455565,"10e"))
-# print(format(44, "5e"))
-# print(format(45,"6e"))
+print(format(455565,"10e"))
+print(format(44, "5e"))
+print(format(45,"6e"))
 
 
 
